8b85bb21dbafceb27001971ec6b7029a3c3cac37
selencimen <selencimen7@gmail.com>
2025-12-15 17:29:46 +0300

    Implement buddy functions and fix style issues

--- today.py
+++ today.py
@@ -1,6 +1,9 @@
+"""Simple git practice functions."""
 def my_name_is():
+    """Return my GitHub username."""
     return "selencimen"
 
 
 def my_buddy_is():
+    """Return my buddy name."""
     return "workintech"
